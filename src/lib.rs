b9b07421bfd540af9679d4ffc2f6c4bbf4638203
Simon Sapin <simon.sapin@exyr.org>
2016-07-16 19:25:08 +0200

    Convert to SystemTime

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
 pub struct Utc;
 
 pub struct DateTime<Tz> {
@@ -27,6 +29,48 @@ impl<Tz> DateTime<Tz> {
     }
 }
 
+impl From<DateTime<Utc>> for SystemTime {
+    fn from(datetime: DateTime<Utc>) -> Self {
+        let days_since_unix =
+            (datetime.year - 1970) * 360
+            + leap_days_since_y0(datetime.year) - leap_days_since_y0(datetime.year)
+            + datetime.month.days_since_january_1st(datetime.year.into())
+            + i32::from(datetime.day - 1);
+        let seconds_since_unix =
+            i64::from(days_since_unix) * SECONDS_PER_DAY
+            + i64::from(datetime.hour) * SECONDS_PER_HOUR
+            + i64::from(datetime.minute) * SECONDS_PER_MINUTE
+            + i64::from(datetime.second);
+        if seconds_since_unix >= 0 {
+            UNIX_EPOCH + Duration::from_secs(seconds_since_unix as u64)
+        } else {
+            UNIX_EPOCH - Duration::from_secs((-seconds_since_unix) as u64)
+        }
+    }
+}
+
+/// How many leap days occured between March of year 0 and January of the given year
+/// (in Gregorian calendar).
+///
+/// March so that we don’t need count Feb 29 of year 0.
+fn leap_days_since_y0(year: i32) -> i32 {
+    let year = year - 1;
+    (year / 4) - (year / 100) + (year / 400)
+}
+
+const SECONDS_PER_MINUTE: i64 = 60;
+const SECONDS_PER_HOUR: i64 = SECONDS_PER_MINUTE * 60;
+const SECONDS_PER_DAY: i64 = SECONDS_PER_HOUR * 24;
+
+/// The Gregorian calendar cycles every 400 years. In one cycle, there are:
+/// * 100 years multiple of 4
+/// * 4 years multiple of 100
+/// * 1 year multiple of 400
+const LEAP_DAYS_PER_400YEARS: i32 = 100 - 4 + 1;
+
+const DAYS_PER_COMMON_YEAR: i32 = 365;
+const DAYS_PER_400YEARS: i32 = DAYS_PER_COMMON_YEAR * 400 + LEAP_DAYS_PER_400YEARS;
+
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum YearKind {
     Common,
@@ -47,23 +91,6 @@ impl From<i32> for YearKind {
     }
 }
 
-const SECONDS_PER_MINUTE: u32 = 60;
-const SECONDS_PER_HOUR: u32 = SECONDS_PER_MINUTE * 60;
-const SECONDS_PER_DAY: u32 = SECONDS_PER_HOUR * 24;
-
-impl YearKind {
-    pub fn days(self) -> u32 {
-        match self {
-            YearKind::Common => 365,
-            YearKind::Leap => 366,
-        }
-    }
-
-    pub fn seconds(self) -> u32 {
-        self.days() * SECONDS_PER_DAY
-    }
-}
-
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Month {
     January,
@@ -81,7 +108,7 @@ pub enum Month {
 }
 
 impl Month {
-    pub fn days(self, year_kind: YearKind) -> u32 {
+    pub fn days(self, year_kind: YearKind) -> i32 {
         match self {
             Month::January => 31,
             Month::February => match year_kind {
@@ -101,11 +128,7 @@ impl Month {
         }
     }
 
-    pub fn seconds(self, year_kind: YearKind) -> u32 {
-        self.days(year_kind) * SECONDS_PER_DAY
-    }
-
-    fn days_since_january(self, year_kind: YearKind) -> u32 {
+    fn days_since_january_1st(self, year_kind: YearKind) -> i32 {
         use Month::*;
         macro_rules! sum {
             ( $( $earlier_month: ident )* ) => {
@@ -127,15 +150,10 @@ impl Month {
             December => sum!(January February March April May June July August September October November),
         }
     }
-
-    fn seconds_since_january(self, year_kind: YearKind) -> u32 {
-        self.days_since_january(year_kind) * SECONDS_PER_DAY
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Add;
     use super::{YearKind, Month};
 
     #[test]
@@ -159,85 +177,35 @@ mod tests {
     }
 
     #[test]
-    fn yearly_seconds() {
-        assert_eq!(YearKind::from(2000).seconds(), 31_622_400);
-        assert_eq!(YearKind::from(2001).seconds(), 31_536_000);
-    }
-
-    #[test]
-    fn monthly_seconds() {
-        // Python
-        // y = 2015
-        // for i in range(1, 12):
-        //   print(i, (datetime.datetime(y, i + 1, 1) - datetime.datetime(y, i, 1)).total_seconds())
-        assert_eq!(Month::January  .seconds(YearKind::Common), 2_678_400);
-        assert_eq!(Month::February .seconds(YearKind::Common), 2_419_200);
-        assert_eq!(Month::March    .seconds(YearKind::Common), 2_678_400);
-        assert_eq!(Month::April    .seconds(YearKind::Common), 2_592_000);
-        assert_eq!(Month::May      .seconds(YearKind::Common), 2_678_400);
-        assert_eq!(Month::June     .seconds(YearKind::Common), 2_592_000);
-        assert_eq!(Month::July     .seconds(YearKind::Common), 2_678_400);
-        assert_eq!(Month::August   .seconds(YearKind::Common), 2_678_400);
-        assert_eq!(Month::September.seconds(YearKind::Common), 2_592_000);
-        assert_eq!(Month::October  .seconds(YearKind::Common), 2_678_400);
-        assert_eq!(Month::November .seconds(YearKind::Common), 2_592_000);
-        assert_eq!(Month::December .seconds(YearKind::Common), 2_678_400);
-
-        assert_eq!(Month::January  .seconds(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::February .seconds(YearKind::Leap), 2_505_600);
-        assert_eq!(Month::March    .seconds(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::April    .seconds(YearKind::Leap), 2_592_000);
-        assert_eq!(Month::May      .seconds(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::June     .seconds(YearKind::Leap), 2_592_000);
-        assert_eq!(Month::July     .seconds(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::August   .seconds(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::September.seconds(YearKind::Leap), 2_592_000);
-        assert_eq!(Month::October  .seconds(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::November .seconds(YearKind::Leap), 2_592_000);
-        assert_eq!(Month::December .seconds(YearKind::Leap), 2_678_400);
-    }
-
-    #[test]
-    fn yearly_monthly_consistency() {
-        use super::Month::*;
-        let twelve = [January, February, March, April, May, June,
-                      July, August, September, October, November, December];
-
-        assert_eq!(twelve.iter().map(|&m| m.seconds(YearKind::Common)).fold(0, Add::add),
-                   YearKind::Common.seconds());
-        assert_eq!(twelve.iter().map(|&m| m.seconds(YearKind::Leap)).fold(0, Add::add),
-                   YearKind::Leap.seconds());
-    }
-
-    #[test]
-    fn seconds_since_january() {
+    fn days_since_january_1st() {
         // Python:
         // ym = [(y, m) for y in [2015, 2016] for m in range(1,13)]
-        // [ll % int((dt(y, m, 1) - dt(y, 1, 1)).total_seconds()) for (ll, (y, m)) in zip(l, ym)]
-        assert_eq!(Month::January  .seconds_since_january(YearKind::Common), 0);
-        assert_eq!(Month::February .seconds_since_january(YearKind::Common), 2_678_400);
-        assert_eq!(Month::March    .seconds_since_january(YearKind::Common), 5_097_600);
-        assert_eq!(Month::April    .seconds_since_january(YearKind::Common), 7_776_000);
-        assert_eq!(Month::May      .seconds_since_january(YearKind::Common), 10_368_000);
-        assert_eq!(Month::June     .seconds_since_january(YearKind::Common), 13_046_400);
-        assert_eq!(Month::July     .seconds_since_january(YearKind::Common), 15_638_400);
-        assert_eq!(Month::August   .seconds_since_january(YearKind::Common), 18_316_800);
-        assert_eq!(Month::September.seconds_since_january(YearKind::Common), 20_995_200);
-        assert_eq!(Month::October  .seconds_since_january(YearKind::Common), 23_587_200);
-        assert_eq!(Month::November .seconds_since_january(YearKind::Common), 26_265_600);
-        assert_eq!(Month::December .seconds_since_january(YearKind::Common), 28_857_600);
+        // [ll % (dt(y, m, 1) - dt(y, 1, 1)).days for (ll, (y, m)) in zip(l, ym)]
+        assert_eq!(Month::January  .days_since_january_1st(YearKind::Common), 0);
+        assert_eq!(Month::February .days_since_january_1st(YearKind::Common), 31);
+        assert_eq!(Month::March    .days_since_january_1st(YearKind::Common), 59);
+        assert_eq!(Month::April    .days_since_january_1st(YearKind::Common), 90);
+        assert_eq!(Month::May      .days_since_january_1st(YearKind::Common), 120);
+        assert_eq!(Month::June     .days_since_january_1st(YearKind::Common), 151);
+        assert_eq!(Month::July     .days_since_january_1st(YearKind::Common), 181);
+        assert_eq!(Month::August   .days_since_january_1st(YearKind::Common), 212);
+        assert_eq!(Month::September.days_since_january_1st(YearKind::Common), 243);
+        assert_eq!(Month::October  .days_since_january_1st(YearKind::Common), 273);
+        assert_eq!(Month::November .days_since_january_1st(YearKind::Common), 304);
+        assert_eq!(Month::December .days_since_january_1st(YearKind::Common), 334);
 
-        assert_eq!(Month::January  .seconds_since_january(YearKind::Leap), 0);
-        assert_eq!(Month::February .seconds_since_january(YearKind::Leap), 2_678_400);
-        assert_eq!(Month::March    .seconds_since_january(YearKind::Leap), 5_184_000);
-        assert_eq!(Month::April    .seconds_since_january(YearKind::Leap), 7_862_400);
-        assert_eq!(Month::May      .seconds_since_january(YearKind::Leap), 10_454_400);
-        assert_eq!(Month::June     .seconds_since_january(YearKind::Leap), 13_132_800);
-        assert_eq!(Month::July     .seconds_since_january(YearKind::Leap), 15_724_800);
-        assert_eq!(Month::August   .seconds_since_january(YearKind::Leap), 18_403_200);
-        assert_eq!(Month::September.seconds_since_january(YearKind::Leap), 21_081_600);
-        assert_eq!(Month::October  .seconds_since_january(YearKind::Leap), 23_673_600);
-        assert_eq!(Month::November .seconds_since_january(YearKind::Leap), 26_352_000);
-        assert_eq!(Month::December .seconds_since_january(YearKind::Leap), 28_944_000);
+        assert_eq!(Month::January  .days_since_january_1st(YearKind::Leap), 0);
+        assert_eq!(Month::February .days_since_january_1st(YearKind::Leap), 31);
+        assert_eq!(Month::March    .days_since_january_1st(YearKind::Leap), 60);
+        assert_eq!(Month::April    .days_since_january_1st(YearKind::Leap), 91);
+        assert_eq!(Month::May      .days_since_january_1st(YearKind::Leap), 121);
+        assert_eq!(Month::June     .days_since_january_1st(YearKind::Leap), 152);
+        assert_eq!(Month::July     .days_since_january_1st(YearKind::Leap), 182);
+        assert_eq!(Month::August   .days_since_january_1st(YearKind::Leap), 213);
+        assert_eq!(Month::September.days_since_january_1st(YearKind::Leap), 244);
+        assert_eq!(Month::October  .days_since_january_1st(YearKind::Leap), 274);
+        assert_eq!(Month::November .days_since_january_1st(YearKind::Leap), 305);
+        assert_eq!(Month::December .days_since_january_1st(YearKind::Leap), 335);
+
     }
 }
