06b5e0d052354225273d94ba584c30777c9c19d6
Simon Sapin <simon.sapin@exyr.org>
2016-07-17 23:12:45 +0200

    Move an impl closer to its type.

--- src/time_zones.rs
+++ src/time_zones.rs
@@ -10,6 +10,29 @@ pub trait TimeZone {
 #[derive(Debug, Eq, PartialEq, Copy, Clone, Default)]
 pub struct Utc;
 
+impl TimeZone for Utc {
+    fn from_timestamp(&self, u: UnixTimestamp) -> NaiveDateTime {
+        let days_since_unix = div_floor(u.0, SECONDS_PER_DAY) as i32;
+        let days = days_since_unix + days_since_d0(1970);
+        let year = div_floor(days * 400, DAYS_PER_400YEARS) as i32;
+        let day_of_the_year = days - days_since_d0(year);
+        let (month, day) = Month::from_day_of_the_year(day_of_the_year, year.into());
+        let hour = positive_rem(div_floor(u.0, SECONDS_PER_HOUR), 24) as u8;
+        let minute = positive_rem(div_floor(u.0, SECONDS_PER_MINUTE), 60) as u8;
+        let second = positive_rem(u.0, 60) as u8;
+        NaiveDateTime::new(year, month, day, hour, minute, second)
+    }
+
+    fn to_timestamp(&self, d: &NaiveDateTime) -> UnixTimestamp {
+        UnixTimestamp(
+            i64::from(days_since_unix(d)) * SECONDS_PER_DAY
+            + i64::from(d.hour) * SECONDS_PER_HOUR
+            + i64::from(d.minute) * SECONDS_PER_MINUTE
+            + i64::from(d.second)
+        )
+    }
+}
+
 /// The offset is typically positive east of Greenwich (longitude 0°), negative west.
 ///
 /// For example, Japan Standard Time is UTC+09:00:
@@ -53,29 +76,6 @@ impl TimeZone for FixedOffsetFromUtc {
         // For positives offsets (ahead of UTC) this is earlier in time than UTC midnight
         // (with more seconds), so *subtract* the offset to make a Unix timestamp.
         UnixTimestamp(seconds - i64::from(self.seconds_ahead_of_utc))
-    }
-}
-
-impl TimeZone for Utc {
-    fn from_timestamp(&self, u: UnixTimestamp) -> NaiveDateTime {
-        let days_since_unix = div_floor(u.0, SECONDS_PER_DAY) as i32;
-        let days = days_since_unix + days_since_d0(1970);
-        let year = div_floor(days * 400, DAYS_PER_400YEARS) as i32;
-        let day_of_the_year = days - days_since_d0(year);
-        let (month, day) = Month::from_day_of_the_year(day_of_the_year, year.into());
-        let hour = positive_rem(div_floor(u.0, SECONDS_PER_HOUR), 24) as u8;
-        let minute = positive_rem(div_floor(u.0, SECONDS_PER_MINUTE), 60) as u8;
-        let second = positive_rem(u.0, 60) as u8;
-        NaiveDateTime::new(year, month, day, hour, minute, second)
-    }
-
-    fn to_timestamp(&self, d: &NaiveDateTime) -> UnixTimestamp {
-        UnixTimestamp(
-            i64::from(days_since_unix(d)) * SECONDS_PER_DAY
-            + i64::from(d.hour) * SECONDS_PER_HOUR
-            + i64::from(d.minute) * SECONDS_PER_MINUTE
-            + i64::from(d.second)
-        )
     }
 }
 
